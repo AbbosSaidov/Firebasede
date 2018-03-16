b7e1e16e63204b133eee1dfd2d85fe51ad38d4ac
insi.game <saidovabbos@gmail.com>
2018-03-16 08:26:33 +0500

    BUyoda firebasega qushish

--- app/src/main/java/com/example/insigame/firebasede/MainActivity.java
+++ app/src/main/java/com/example/insigame/firebasede/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.insigame.firebasede;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText=findViewById(R.id.editText);
+    }
+
+
+    public void SendData(View view){
+       // Firebase firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+    }
+
+    public void Yubor(View view){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+
+        myRef.setValue(editText.getText().toString());
+
     }
 }
