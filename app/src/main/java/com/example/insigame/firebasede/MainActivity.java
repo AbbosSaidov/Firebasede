9966b43c35ec46af1e1191c9d347af4e194e134a
insi.game <saidovabbos@gmail.com>
2018-03-16 22:53:27 +0500

    BUyoda firebasega дшыеутпф рфкфлфе

--- app/src/main/java/com/example/insigame/firebasede/MainActivity.java
+++ app/src/main/java/com/example/insigame/firebasede/MainActivity.java
@@ -5,20 +5,48 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editText;
-
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         editText=findViewById(R.id.editText);
+        textView=findViewById(R.id.textView2);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+/**/
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                textView.setText(value);
+             //   Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+             ///
+                textView.setText("Failed to read value."+ error.toException());
+             ///   Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
     }
 
 
@@ -34,8 +62,8 @@ public class MainActivity extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
 
-
-        myRef.setValue(editText.getText().toString());
+        textView.setText(myRef.getKey());
+        myRef.push().setValue(editText.getText().toString());
 
     }
 }
